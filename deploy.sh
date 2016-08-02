72ffb1c8f6681feb723173ef90083b72e89ee36b
mfo <fourcade.m@gmail.com>
2016-08-02 14:36:08 +0200

    tech(deploy): with migrate and restart

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,13 @@
 #                then execute
 
 set -x
+$HEROKU_APP_NAME=resa-liberte-paris
 
+echo "Push to heroku"
 git push heroku master:master
+
+echo "Run migration"
+heroku run rake db:migrate -a $HEROKU_APP_NAME
+
+echo "Restart"
+heroku restart -a $HEROKU_APP_NAME
