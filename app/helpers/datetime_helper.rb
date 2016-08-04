3c2e5f5ad04f3db0f431f221cf23b6aed6b74272
mfo <fourcade.m@gmail.com>
2016-08-04 09:49:36 +0200

    test(datetime_heelpers): make sure it is rock solid

--- app/helpers/datetime_helper.rb
+++ app/helpers/datetime_helper.rb
@@ -1,10 +1,10 @@
 # Serialize / Deserialize dates
 module DatetimeHelper
-  def date_to_param(datetime)
+  def date_to_param(date)
     {
-      year:    datetime.strftime('%Y'),
-      month:   datetime.strftime('%m'),
-      day:     datetime.strftime('%d')
+      year:    date.strftime('%Y'),
+      month:   date.strftime('%m'),
+      day:     date.strftime('%d')
     }
   end
 
---
+++ test/helpers/datetime_helper_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class DatetimeHelperTest < ActionView::TestCase
+  setup do
+    @year = 2016
+    @month = 1
+    @day = 1
+    @hour = 1
+    @minute = 1
+    @date = Time.zone.local(@year, @month, @day, @hour, @minute)
+  end
+
+  test 'date_to_param serialize year, month and day with 0 padding of 2' do
+    date_hash = {
+      year: format('%d', @year),
+      month: format('%02d', @month),
+      day: format('%02d', @day)
+    }
+    assert_equal date_hash, date_to_param(@date)
+  end
+
+  test 'datetime_to_param serialize year and m,d,h,m with 0 padding of 2' do
+    datetime_hash = {
+      year: format('%d', @year),
+      month: format('%02d', @month),
+      day: format('%02d', @day),
+      hour: format('%02d', @hour),
+      minute: format('%02d', @minute)
+    }
+    assert_equal datetime_hash, datetime_to_param(@date)
+  end
+
+  test 'datetime_from_param works with datetime_to_param' do
+    assert_equal @date, datetime_from_param(datetime_to_param(@date))
+  end
+end
