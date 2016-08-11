324363efd03f8cbb194ba10972bb89e175826c9e
mfo <fourcade.m@gmail.com>
2016-08-11 10:36:33 +0200

    tech(locales): fix issue and colocate definitions

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,8 @@ module ResaParisLiberte
     config.active_job.queue_adapter = :delayed_job
 
     # I18n
-    config.i18n.available_locales = %i(fr)
+    config.i18n.available_locales = [:fr]
+    config.i18n.locale = :fr
+    config.i18n.default_locale = :fr
   end
 end
--- config/initializers/locale.rb
+++
@@ -1,2 +0,0 @@
-I18n.locale = :fr
-I18n.default_locale = :fr
