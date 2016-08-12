af36f322cb57c7fa7420daa55206ea497ad6a2ed
mfo <fourcade.m@gmail.com>
2016-08-12 08:46:45 +0200

    test(datetime.date_to_param): with in/out serialization

--- test/helpers/datetime_helper_test.rb
+++ test/helpers/datetime_helper_test.rb
@@ -7,7 +7,8 @@ class DatetimeHelperTest < ActionView::TestCase
     @day = 1
     @hour = 1
     @minute = 1
-    @date = Time.zone.local(@year, @month, @day, @hour, @minute)
+    @datetime = Time.zone.local(@year, @month, @day, @hour, @minute)
+    @date = Time.zone.local(@year, @month, @day)
   end
 
   test 'date_to_param serialize year, month and day with 0 padding of 2' do
@@ -16,7 +17,7 @@ class DatetimeHelperTest < ActionView::TestCase
       month: format('%02d', @month),
       day: format('%02d', @day)
     }
-    assert_equal date_hash, date_to_param(@date)
+    assert_equal date_hash, date_to_param(@datetime)
   end
 
   test 'datetime_to_param serialize year and m,d,h,m with 0 padding of 2' do
@@ -27,10 +28,14 @@ class DatetimeHelperTest < ActionView::TestCase
       hour: format('%02d', @hour),
       minute: format('%02d', @minute)
     }
-    assert_equal datetime_hash, datetime_to_param(@date)
+    assert_equal datetime_hash, datetime_to_param(@datetime)
   end
 
   test 'datetime_from_param works with datetime_to_param' do
-    assert_equal @date, datetime_from_param(datetime_to_param(@date))
+    assert_equal @datetime, datetime_from_param(datetime_to_param(@datetime))
+  end
+
+  test 'date_from_param works with date_to_param' do
+    assert_equal @date, date_from_param(date_to_param(@date))
   end
 end
