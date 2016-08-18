3a8d6a3c5a725bf0df1bc0d9561bccf322adddeb
mfo <fourcade.m@gmail.com>
2016-08-18 11:18:28 +0200

    fix(turbolinks): wrong redirection when not signed in and linked clicked from homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def disable_turbolink_only_on_signin
+    if resident_signed_in?
+      true
+    elsif !current_page?(root_url)
+      true
+    else
+      false
+    end
+  end
 end
