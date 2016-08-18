34c25bdce9585559c97f6c6428062e9c0a83a347
mfo <fourcade.m@gmail.com>
2016-08-18 14:50:06 +0200

    test(homepage#signed_in): ensure it redirects to rooms_path

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  fixtures :residents
+
   test 'should get home on request root_url' do
     get root_url
     assert_response :success
+  end
+
+  test 'should redirect to rooms_url when logged in' do
+    sign_in(residents(:mfo))
+    get root_url
+    assert_redirected_to rooms_url
   end
 end
