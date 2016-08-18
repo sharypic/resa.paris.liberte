4dd58fe68581fdd8f808633697c8d6d125161c19
mfo <fourcade.m@gmail.com>
2016-08-18 15:00:31 +0200

    feat(admin#dashboards): redirect to teams

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -2,6 +2,7 @@ module Admin
   # uniq interface to manage them all
   class DashboardsController < Admin::ApplicationController
     def index
+      redirect_to admin_teams_path
     end
   end
 end
