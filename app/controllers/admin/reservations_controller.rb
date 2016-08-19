701d757922877f610f6c8acefa38e2f52536ee63
mfo <fourcade.m@gmail.com>
2016-08-19 14:49:52 +0200

    feat(admin#reservations/index): allow user to filter them by dates

--- app/controllers/admin/reservations_controller.rb
+++ app/controllers/admin/reservations_controller.rb
@@ -2,9 +2,16 @@ module Admin
   # list team reservations
   class ReservationsController < Admin::ApplicationController
     before_action :set_team
+    include DateParser
 
     def index
-      @reservations = @team.reservations
+      from_date = date_or_default(params, 'from_')
+      to_date = date_or_default(params, 'to_')
+      to_date = from_date + 1.week if from_date == to_date
+
+      @reservations = @team.reservations.in_range((from_date..to_date))
+
+      render locals: { from_date: from_date, to_date: to_date }
     end
 
     private
